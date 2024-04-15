187b727b0790e545f7cf3f404e207d24245af54d
Jonathan Zamora <50164418+jonzamora@users.noreply.github.com>
2024-04-15 04:48:38 -0700

    clean setup.py

--- setup.py
+++ setup.py
@@ -2,25 +2,23 @@
 setuptools script for dmc2gymnasium.
 """
 
-# Always prefer setuptools over distutils
+
 from setuptools import setup, find_packages
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
-
-# Get the long description from the README file
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
-    name="dmc2gymnasium",  # Required
-    version="1.0.0",  # Required
-    description="DeepMind Control Suite to Farama Gymnasium Wrapper",  # Optional
-    long_description=long_description,  # Optional
-    long_description_content_type="text/markdown",  # Optional (see note above)
-    url="https://github.com/jonzamora/dmc2gymnasium",  # Optional
-    author="Jonathan Zamora",  # Optional
-    author_email="jonathan.zamora@usc.edu",  # Optional
-    keywords="dm_control, gymnasium, gym",  # Optional
-    packages=find_packages(),  # Required
-    install_requires=["gymnasium", "dm_control", "mujoco", "moviepy"],  # Optional
+    name="dmc2gymnasium",
+    version="1.0.0",
+    description="Farama Gymnasium API Wrapper for DeepMind Control Suite",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jonzamora/dmc2gymnasium",
+    author="Jonathan Zamora",
+    author_email="jonathan.zamora@usc.edu",
+    keywords="dm_control, gymnasium, gym",
+    packages=find_packages(),
+    install_requires=["gymnasium", "dm_control", "mujoco", "moviepy"],
 )
